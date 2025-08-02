d91fc4ceaf2fc574151dd3615c21829ef3b6a979
web3lookatme <mycbemail2021@gmail.com>
2025-08-02 06:27:22 -0400

    fix: Correct typo in postcss config

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,10 +2,7 @@ const path = require('path');
 
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {
-      // Explicitly point to the Tailwind config file using an absolute path
-      config: path.resolve(__dirname, 'tailwind.config.cjs')
-    },
-    'autoprefixer': {},
+    tailwindcss: {},
+    autoprefixer: {},
   },
 }
